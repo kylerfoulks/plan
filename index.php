b36019b3acdb1c11d7d8e0c13cae7d1a6cfc6f15
kylerfoulks <kylerfoulks@gmail.com>
2015-10-10 20:25:22 -0400

    Initial upload
    initial upload of landing page. It begins here!

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+ <head>
+  <title>Web Planner Landing Page</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+ </head>
+ <body>
+ 
+ 	<div id="landingLogo"></div>
+ 	<div id="landingForm">
+	 	<form>
+	 		<input type="text" placeholder="Email Address"> <br>
+	 		<input type="password" placeholder="Password"> <br>
+	 		<input type="password" placeholder="Confirm Password"><br> 
+	 		<input class="landingSubmit" type="submit">
+	 	</form>
+ 	</div>
+<span class="landingTagline">Website collaboration made simple.</span>
+
+
+ </body>
+</html>
